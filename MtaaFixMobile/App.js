4e5645a221af703fea040e3f68f73d3ecdf1755f
Carey99 <edwinakidah1@gmail.com>
2026-07-30 19:15:57 +0300

    wrapped navigation container under auth provider

--- MtaaFixMobile/App.js
+++ MtaaFixMobile/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { Keyboard, TouchableWithoutFeedback, View } from 'react-native';
 import AppNavigator from './src/navigation/AppNavigator';
+import { AuthProvider } from './src/context/AutthContext'
 
 export default function App() {
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
       <View style={{ flex: 1 }}>
-        <NavigationContainer>
-          <AppNavigator />
-        </NavigationContainer>
+        <AuthProvider>
+          <NavigationContainer>
+            <AppNavigator />
+          </NavigationContainer>
+        </AuthProvider>
       </View>
     </TouchableWithoutFeedback>
   );
